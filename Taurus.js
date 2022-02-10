0b96f8fb7d80c97c445f7c16f40b0b443aa990d7
MUHAMMED <93420716+I-AM-MUHAMMED@users.noreply.github.com>
2022-02-10 21:20:13 +0530

    G

--- Taurus.js
+++ Taurus.js
@@ -291,7 +291,6 @@ module.exports = bosco = async (bosco, mek) => {
 		pporang = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 		      }
 		const taurus = await getBuffer(pporang)
-		const dfrply = await getBuffer(pporang)
 		
 		        
         const isUrl = (url) => {
@@ -4643,6 +4642,3 @@ console.log('[',color('TEXT','teal'),']',`Message : ${budy} From`, color(pushnam
         }
 	}
 }
-
-
-
